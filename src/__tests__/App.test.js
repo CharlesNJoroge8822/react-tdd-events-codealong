7e6ae0b05eb6f1657753a4496a7102c435cc1655
njorogecharles439@gmail.com <njorogecharles439@gmail.com.>
2024-11-11 08:27:15 +0300

    done

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,13 @@ import App from "../App";
 import '@testing-library/jest-dom';
 
 // Code tests here
+
+test ('renders a header and a paragraph', () => {
+
+    render (<App />)
+
+    // test header 
+    const pageHeader = screen.getByText (/Lorem Ipsum/)
+
+});
+
